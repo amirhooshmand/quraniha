344bb7d63992bef1da401a6dea46205c74da8b9e
Amir Hossein Hooshmand <amir.hsmart@gmail.com>
2020-05-04 21:13:00 +0430

    add header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react'
+import NavigationContainer from './components/Navigation/NavigationContainer'
+import './css/App.css'
+import FooterContainer from './components/Footer/FooterContainer'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
-function App() {
-  return (
-    <div>
-      sfd 
-    </div>
-  );
+class App extends Component {
+
+  state = { "location": "/" }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          
+
+          <Route exact path="/" render={() => <Home />} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+
+          <FooterContainer />
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
